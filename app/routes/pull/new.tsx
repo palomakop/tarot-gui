ffaf82f46ec0fce87b35d98ab6d7e1f842dd4ece
PK <doubleobelisk@gmail.com>
2025-03-12 19:07:27 -0400

    working on the form!

--- app/routes/pull/new.tsx
+++ app/routes/pull/new.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "../+types/home";
-import { NewPullForm } from "../../components/newPullForm"
+import axios from 'axios';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,7 +9,40 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function NewPull() {
+
+  function handleSubmit(data: FormData): void {
+    console.log("Form submitted");
+  }
+
   return (
-    <NewPullForm />
+    <div className="flex flex-col items-center justify-center p-2 sm:p-4 gap-6 w-[500px] max-w-full ml-auto mr-auto">
+      <h1>
+        New Tarot Pull
+      </h1>
+      <form action={handleSubmit}>
+        <p>Choose a card layout:</p>
+        <div className="radio">
+          <input type="radio" name="layout" value="Single" defaultChecked />
+          <label htmlFor="Single">Single</label>
+        </div>
+        <div className="radio">
+          <input type="radio" name="layout" value="Triple" />
+          <label htmlFor="css">Triple</label>
+        </div>
+        <div className="radio">
+          <input type="radio" name="layout" value="Cross" />
+          <label htmlFor="Cross">Cross</label>
+        </div>
+        <p className="checkbox">
+          <input type="checkbox" name="AllowReversed" value="AllowReversed" defaultChecked />
+          <label htmlFor="AllowReversed">Allow reversed cards?</label>
+        </p>
+        <p>
+          <label htmlFor="Intention">Briefly set an intention:</label>
+        </p>
+        <input type="text" maxLength={50} name="Intention" placeholder="Optional" />
+        <button type="submit">Pull Cards</button>
+      </form>
+    </div>
   );
 }
